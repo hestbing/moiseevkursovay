d46a77087e27d3a71cead3d66a7a1f1fcd9243c4
Дмитрий Моисеев <108190091+hestbing@users.noreply.github.com>
2022-12-19 05:36:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.resize(520,700);
+    w.setWindowTitle("Список услуг");
     w.show();
     return a.exec();
 }
